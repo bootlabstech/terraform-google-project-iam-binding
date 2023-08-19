39ad8a40b261307fa9f22202af0a9a7c8dcb61e1
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-19 17:23:43 +0530

    multiple roles

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "google_project_iam_binding" "binding" {
-  for_each = { for binding in var.google_project_iam_bindings : binding.unique => binding }
-  project  = each.value.project
-  role     = each.value.role
-  members  = each.value.members
+for_each = toset(var.roles)
+  project  = var.project_id
+  role     = each.key
+  members  = var.members
   lifecycle {
     ignore_changes = [etag]
   }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "google_project_iam_bindings" {
-  type = list(object({
-    unique  = number,
-    project = string,
-    role    = string,
-    members = list(string)
-  }))
-  description = "List of project iam members."
+variable "project_id" {
+  type = string
+}
+variable "roles" {
+  type = list(string)
+}
+variable "members" {
+  type = list(string)
 }
